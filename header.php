143bbe3ab8f63b62076903a5ba0548217ddb42ca
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-05-11 17:36:10 -0500

    moved content into content templates

--- header.php
+++ header.php
@@ -20,17 +20,4 @@
 <!-- Nav bar sourcing in PHP goes right here -->
 <?php get_template_part('navigation'); ?>
     <!-- Header -->
-    <header>
-        <div class="container" id="maincontent" tabindex="-1">
-            <div class="row">
-                <div class="col-lg-12">
-                    <img class="img-responsive" src="img/profile.png" alt="">
-                    <div class="intro-text">
-                        <h1 class="name"><?php echo get_bloginfo( 'name' ); ?></h1>
-                        <hr class="star-light">
-                        <span class="skills"><?php echo get_bloginfo( 'description' ); ?></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
+<?php get_template_part('hero-header'); ?>
---
+++ hero-header.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container" id="maincontent" tabindex="-1">
+        <div class="row">
+            <div class="col-lg-12">
+                <img class="img-responsive" src="img/profile.png" alt="">
+                <div class="intro-text">
+                    <h1 class="name"><?php echo get_bloginfo( 'name' ); ?></h1>
+                    <hr class="star-light">
+                    <span class="skills"><?php echo get_bloginfo( 'description' ); ?></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
